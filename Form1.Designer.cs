963a3ee15ccaac720b1771ad0e33030486dcaff5
AndjeloR <obervk1003@gmail.com>
2026-04-23 16:38:54 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace CookingRecipeManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lstRecipes = new System.Windows.Forms.ListBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lbRecipeTitle = new System.Windows.Forms.Label();
+            this.lbIngredients = new System.Windows.Forms.Label();
+            this.lbInstructions = new System.Windows.Forms.Label();
+            this.txtIngredients = new System.Windows.Forms.TextBox();
+            this.txtInstructions = new System.Windows.Forms.TextBox();
+            this.pbRecipeImage = new System.Windows.Forms.PictureBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.cmbSort = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRecipeImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.BurlyWood;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1495, 176);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label1.Location = new System.Drawing.Point(622, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digital Cookbook";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.cmbSort);
+            this.panel2.Controls.Add(this.lstRecipes);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 176);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(223, 763);
+            this.panel2.TabIndex = 1;
+            // 
+            // lstRecipes
+            // 
+            this.lstRecipes.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.lstRecipes.ForeColor = System.Drawing.Color.Black;
+            this.lstRecipes.FormattingEnabled = true;
+            this.lstRecipes.ItemHeight = 30;
+            this.lstRecipes.Location = new System.Drawing.Point(0, 0);
+            this.lstRecipes.Name = "lstRecipes";
+            this.lstRecipes.Size = new System.Drawing.Size(223, 724);
+            this.lstRecipes.TabIndex = 0;
+            this.lstRecipes.SelectedIndexChanged += new System.EventHandler(this.lstRecipes_SelectedIndexChanged);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Controls.Add(this.btnClear);
+            this.panel3.Controls.Add(this.btnUpload);
+            this.panel3.Controls.Add(this.btnDelete);
+            this.panel3.Controls.Add(this.btnSave);
+            this.panel3.Controls.Add(this.pbRecipeImage);
+            this.panel3.Controls.Add(this.txtInstructions);
+            this.panel3.Controls.Add(this.txtIngredients);
+            this.panel3.Controls.Add(this.lbInstructions);
+            this.panel3.Controls.Add(this.lbIngredients);
+            this.panel3.Controls.Add(this.lbRecipeTitle);
+            this.panel3.Controls.Add(this.txtName);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(223, 176);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1272, 763);
+            this.panel3.TabIndex = 2;
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Segoe UI", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(37, 60);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(673, 22);
+            this.txtName.TabIndex = 0;
+            // 
+            // lbRecipeTitle
+            // 
+            this.lbRecipeTitle.AutoSize = true;
+            this.lbRecipeTitle.ForeColor = System.Drawing.Color.Black;
+            this.lbRecipeTitle.Location = new System.Drawing.Point(37, 15);
+            this.lbRecipeTitle.Name = "lbRecipeTitle";
+            this.lbRecipeTitle.Size = new System.Drawing.Size(124, 30);
+            this.lbRecipeTitle.TabIndex = 1;
+            this.lbRecipeTitle.Text = "Recipe Title";
+            // 
+            // lbIngredients
+            // 
+            this.lbIngredients.AutoSize = true;
+            this.lbIngredients.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbIngredients.ForeColor = System.Drawing.Color.Black;
+            this.lbIngredients.Location = new System.Drawing.Point(37, 109);
+            this.lbIngredients.Name = "lbIngredients";
+            this.lbIngredients.Size = new System.Drawing.Size(122, 30);
+            this.lbIngredients.TabIndex = 2;
+            this.lbIngredients.Text = "Ingredients";
+            // 
+            // lbInstructions
+            // 
+            this.lbInstructions.AutoSize = true;
+            this.lbInstructions.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.lbInstructions.ForeColor = System.Drawing.Color.Black;
+            this.lbInstructions.Location = new System.Drawing.Point(38, 390);
+            this.lbInstructions.Name = "lbInstructions";
+            this.lbInstructions.Size = new System.Drawing.Size(123, 30);
+            this.lbInstructions.TabIndex = 3;
+            this.lbInstructions.Text = "Instructions";
+            // 
+            // txtIngredients
+            // 
+            this.txtIngredients.Location = new System.Drawing.Point(37, 151);
+            this.txtIngredients.Multiline = true;
+            this.txtIngredients.Name = "txtIngredients";
+            this.txtIngredients.Size = new System.Drawing.Size(673, 187);
+            this.txtIngredients.TabIndex = 4;
+            // 
+            // txtInstructions
+            // 
+            this.txtInstructions.Location = new System.Drawing.Point(37, 432);
+            this.txtInstructions.Multiline = true;
+            this.txtInstructions.Name = "txtInstructions";
+            this.txtInstructions.Size = new System.Drawing.Size(1146, 261);
+            this.txtInstructions.TabIndex = 5;
+            // 
+            // pbRecipeImage
+            // 
+            this.pbRecipeImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbRecipeImage.Location = new System.Drawing.Point(773, 60);
+            this.pbRecipeImage.Name = "pbRecipeImage";
+            this.pbRecipeImage.Size = new System.Drawing.Size(440, 278);
+            this.pbRecipeImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pbRecipeImage.TabIndex = 6;
+            this.pbRecipeImage.TabStop = false;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Snow;
+            this.btnSave.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(43, 713);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(140, 38);
+            this.btnSave.TabIndex = 7;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Snow;
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(213, 713);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(140, 38);
+            this.btnDelete.TabIndex = 8;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.BackColor = System.Drawing.Color.Snow;
+            this.btnUpload.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpload.Location = new System.Drawing.Point(934, 344);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(140, 38);
+            this.btnUpload.TabIndex = 9;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = false;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Snow;
+            this.btnClear.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(375, 713);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(140, 38);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // cmbSort
+            // 
+            this.cmbSort.FormattingEnabled = true;
+            this.cmbSort.Items.AddRange(new object[] {
+            "A - Z",
+            "Z - A"});
+            this.cmbSort.Location = new System.Drawing.Point(102, 725);
+            this.cmbSort.Name = "cmbSort";
+            this.cmbSort.Size = new System.Drawing.Size(121, 38);
+            this.cmbSort.TabIndex = 1;
+            this.cmbSort.SelectedIndexChanged += new System.EventHandler(this.cmbSort_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.label2.Location = new System.Drawing.Point(12, 728);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 30);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Sort by:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(768, 348);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 30);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Upload Picture:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 30F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1495, 939);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbRecipeImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ListBox lstRecipes;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtIngredients;
+        private System.Windows.Forms.Label lbInstructions;
+        private System.Windows.Forms.Label lbIngredients;
+        private System.Windows.Forms.Label lbRecipeTitle;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtInstructions;
+        private System.Windows.Forms.PictureBox pbRecipeImage;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cmbSort;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CookingRecipeManager
+{
+    public partial class Form1 : Form
+    {
+
+        List<Recipe> recipes = new List<Recipe>();
+        string selectedImagePath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            cmbSort.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog ofd = new OpenFileDialog())
+            {
+                ofd.Filter = "Images|*.jpg;*.jpeg;*.png";
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    selectedImagePath = ofd.FileName;
+                    pbRecipeImage.Image = Image.FromFile(selectedImagePath);
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtName.Text)) return;
+
+            var newRecipe = new Recipe
+            {
+                Name = txtName.Text,
+                Ingredients = txtIngredients.Text,
+                Instructions = txtInstructions.Text,
+                ImagePath = selectedImagePath
+            };
+
+            recipes.Add(newRecipe);
+            ApplySorting();
+            ClearInput();
+        }
+
+        private void lstRecipes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lstRecipes.SelectedItem is Recipe selected)
+            {
+                txtName.Text = selected.Name;
+                txtIngredients.Text = selected.Ingredients;
+                txtInstructions.Text = selected.Instructions;
+
+                if (!string.IsNullOrEmpty(selected.ImagePath))
+                    pbRecipeImage.Image = Image.FromFile(selected.ImagePath);
+                else
+                    pbRecipeImage.Image = null;
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (lstRecipes.SelectedItem is Recipe selected)
+            {
+                recipes.Remove(selected);
+                RefreshList();
+                ClearInput();
+            }
+        }
+
+        private void RefreshList()
+        {
+            lstRecipes.DataSource = null;
+            lstRecipes.DataSource = recipes;
+        }
+
+        private void ClearInput()
+        {
+            txtName.Clear();
+            txtIngredients.Clear();
+            txtInstructions.Clear();
+            pbRecipeImage.Image = null;
+            selectedImagePath = "";
+        }
+
+        private void ApplySorting()
+        {
+            if (cmbSort.SelectedItem == null) return;
+
+            string sortOrder = cmbSort.SelectedItem.ToString();
+
+            if (sortOrder == "A - Z")
+            {
+                recipes = recipes.OrderBy(r => r.Name).ToList();
+            }
+            else if (sortOrder == "Z - A")
+            {
+                recipes = recipes.OrderByDescending(r => r.Name).ToList();
+            }
+
+            RefreshList();
+        }
+
+        private void cmbSort_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            ApplySorting();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearInput();
+        }
+    }
+}
